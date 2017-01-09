0de9373767ebfcd61a18b4b8fc5ff3c5c6a9f7bf
Karl Horky <karl.horky@gmail.com>
2017-01-09 18:06:19 +0100

    Change routes file to JavaScript instead of JSON

---
+++ packages/react-scripts/kununu/template/routes.js
@@ -0,0 +1,16 @@
+// Yay! You've scaffolded a universal app! To add some routes, create a module using the following guide.
+//
+// 1. Create a folder and package.json for your example module like in https://github.com/kununu/example-review-module.
+// 2. Move all contents out of the `_moduleTemplate` folder into your module folder and delete the `_moduleTemplate` folder.
+// 3. Link your module to make it available under the `node_modules` directory.
+// 4. Update the routes below to use your new module.
+//
+module.exports = {
+  middleware: [],
+  routes: {
+    Homepage: {
+      path: '/',
+      page: '_moduleTemplate/pages/Homepage.js',
+    },
+  },
+};
