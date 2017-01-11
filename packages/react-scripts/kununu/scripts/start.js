8b032fe91079fa7d9a87d21bca05511818e4753d
Johannes Ewald <johannes.ewald@peerigon.com>
2017-01-11 18:07:53 +0100

    Default to 'development' if no NODE_ENV is set

--- packages/react-scripts/kununu/scripts/start.js
+++ packages/react-scripts/kununu/scripts/start.js
@@ -1,5 +1,8 @@
 const spawn = require("cross-spawn");
 
+// Default to development if no ENV is set
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
 const result = spawn.sync(
     "node",
     [require.resolve("react-server-cli/target/cli"), "start"],
