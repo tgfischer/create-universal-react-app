bb856810fa5c0ff0ea0d3339bcb7fc7112081003
Johannes Ewald <johannes.ewald@peerigon.com>
2017-01-11 18:14:05 +0100

    Add resolveFromReactServerCli helper function
    This helper function is necessary because we need to resolve some dependencies from the perspective of react-server-cli in order to do instanceof and === checks on the webpack config.

--- packages/react-scripts/kununu/utils/webpack/resolveFromReactServerCli.js
+++ packages/react-scripts/kununu/utils/webpack/resolveFromReactServerCli.js
@@ -1,6 +1,13 @@
 const resolve = require("resolve");
 const path = require("path");
 
+/**
+ * This helper function is necessary because we need to resolve some dependencies from the perspective of
+ * react-server-cli in order to do instanceof and === checks on the webpack config.
+ *
+ * @param {string} module
+ * @returns {string}
+ */
 function resolveFromReactServerCli(module) {
     const pathToReactServerCli = require.resolve("react-server-cli");
     
